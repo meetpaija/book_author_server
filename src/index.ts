561a030f5ab700559b02702147751488b7a672b1
meetpaija <meet.paija@gmail.com>
2025-06-20 12:36:12 +0530

    fixing few issues

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import './config/sequelize.ts';
+import './config';
 
 const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
   type Book {
     id: ID!
     title: String
     author: String
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     getBooks: [Book],
     getFirstBook(id: ID!): Book,
@@ -23,43 +17,24 @@ const typeDefs = `#graphql
 `;
 
 const books = [
-  {
-    id: 1,
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    id: 2,
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
+  { id: 1, title: 'The Awakening', author: 'Kate Chopin' },
+  { id: 2, title: 'City of Glass', author: 'Paul Auster' },
 ];
 
-
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
     getBooks: () => books,
-    getFirstBook: (parent: any, { id }: any) => books[id],
-    getBookByID: (parent: any, { id }: any) => books.find(book => book.id === id),
+    getFirstBook: (_: any, { id }: any) => books[0],
+    getBookByID: (_: any, { id }: any) => books.find(book => book.id === +id),
   },
 };
 
+async function main() {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 }
+  });
+  console.log(`🚀  Server ready at ${url}`);
+}
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
-
-console.log(`🚀  Server ready at: ${url}`);
+main();
